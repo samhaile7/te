a7dda38e47b382e68390a168f73ed57bad3ca26b
Samuel Haile <samhaile129@gmail.com>
2023-12-04 18:33:25 -0500

    test

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ async function run() {
     core.info(
       `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
     )
-    const testVar = "sfgfgdgdfg"
+    const testVar = 'sfgfgdgdfg'
     core.exportVariable('HELLO', testVar)
   } catch (error) {
     // Fail the workflow step if an error occurs
