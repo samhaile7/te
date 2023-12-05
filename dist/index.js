dd18360fcd872e1fe0b82a9da9e54b1d29052bce
Samuel Haile <samhaile129@gmail.com>
2023-12-05 07:18:25 -0500

    test

--- dist/index.js
+++ dist/index.js
@@ -28495,11 +28495,10 @@ async function run() {
     core.info(
       `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
     )
-    const testVar = 'sfgfgdgdfg'
+    const testVar = 'AAAAAAA'
     console.log(testVar, "4444444")
-    core.setSecret(testVar)
     console.log(testVar, "5555555555")
-    core.exportVariable('HELLO', testVar)
+    core.exportVariable('HELLO', core.setSecret(testVar))
   } catch (error) {
     // Fail the workflow step if an error occurs
     core.setFailed(error.message)
